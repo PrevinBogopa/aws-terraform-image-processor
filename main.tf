2dfe99bead5242f9ceebfecfa01566317278b863
PrevinBogopa <previnbogopa@icloud.com>
2026-02-20 19:14:43 +0200

    frontend_public

--- main.tf
+++ main.tf
@@ -30,3 +30,25 @@ index_document{
     suffix = "index.html"
 }
 }
+
+resource "aws_s3_bucket_public_access_block" "frontend_public" {
+    bucket = aws_s3_bucket.frontend_bucket.id
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "frontend_policy" {
+    bucket = aws_s3_bucket.frontend_bucket.id
+
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Effect = "Allow"
+            Principal = "*"
+            Action = ["s3:GetObject"]
+            Resource= "${aws_s3_bucket.frontend_bucket.arn}/*"
+        }]
+    })
+}
