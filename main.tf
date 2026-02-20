9be428060282c5ec27b4143984c8ecd2b0b1be07
PrevinBogopa <previnbogopa@icloud.com>
2026-02-20 19:14:13 +0200

    init resources

--- main.tf
+++ main.tf
@@ -10,3 +10,23 @@ terraform{
 provider "aws" {
     region = "us-east-1"
 }
+
+resource "aws_s3_bucket" "upload_bucket" {
+    bucket = "image-upload-bucket-12345"
+}
+
+resource "aws_s3_bucket" "compressed_bucket" {
+    bucket = "image-compressed-bucket-12345"
+}
+
+resource "aws_s3_bucket" "frontend_bucket" {
+    bucket = "image-frontend-bucket-12345"
+}
+
+resource "aws_s3_bucket_website_configuration" "frontend_config" {
+bucket = aws_s3_bucket.frontend_bucket.id 
+
+index_document{
+    suffix = "index.html"
+}
+}
