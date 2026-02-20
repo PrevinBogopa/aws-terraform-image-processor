a973e513758cb29181f4c555a4eefded85f114b1
PrevinBogopa <previnbogopa@icloud.com>
2026-02-20 19:14:52 +0200

    Add Iam ROle

--- main.tf
+++ main.tf
@@ -52,3 +52,20 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
         }]
     })
 }
+# IAM ROLE
+
+resource "aws_iam_role" "lambda_role" {
+
+name = "image_lambda_role" 
+
+assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+            Service = "lambda.amazonaws.com"
+        }
+    }]
+})
+}
