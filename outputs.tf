169e827ea80275bff2af05c24b0953a9e2767c30
PrevinBogopa <previnbogopa@icloud.com>
2026-02-20 19:16:17 +0200

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "frontend_url" {
+    value= aws_s3_bucket_website_configuration.frontend_config.website_endpoint
+}
+
+output "lambda_function_url" {
+  value = aws_lambda_function_url.function_url.function_url
+}
